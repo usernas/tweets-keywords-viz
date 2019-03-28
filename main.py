42371c7c5b838ff3c39f3c4175222f56d1b7ce17
E178298C <max@DESKTOP-RNTNKSO.localdomain>
2019-03-28 18:31:08 +0100

    cred setup

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #!chemin
+"""
+Twitter API will be use to search for tweets containing a single keyword given by the user and 
+then extract more keywords that frequently appears in these tweets.
+ And i will finally visualize these datas on webpage as a point cloud or using js animation.
+"""
+
 import os
 
 rep = input("Entrez le mot-clé à analyser : ")
